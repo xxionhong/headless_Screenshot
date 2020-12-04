ff57b760d9c514b6346eb622ce3949db1342f695
xxion <monkey850416@gmail.com>
2020-12-04 17:54:30 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from Screen_shot import Screen_shot as ss
 
 a = ss()
-b = a.Full_screenshot(url='https://tw.yahoo.com/',save_path='F:/new_pr_app/',image_name='test.png')
+b = a.Full_screenshot(url='https://tw.yahoo.com/',save_path='.',image_name='test.png')
 a.End_selenium()
